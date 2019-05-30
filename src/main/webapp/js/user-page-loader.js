096ce10d76093aae295f292b43baaa1ecd650449
hxcyon <halxcyon@gmail.com>
2019-05-30 16:43:41 +0800

    Add AboutMe Datastore logic

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -103,7 +103,7 @@ function fetchAboutMe(){
     const url = '/about?user=' + parameterUsername;
     fetch(url).then((response) => {
         return response.text();
-    }).then((aboutME) => {
+    }).then((aboutMe) => {
         const aboutMeContainer = document.getElementById('about-me-container');
         if(aboutMe == ''){
             aboutMe = 'This user has not entered any information yet.';
