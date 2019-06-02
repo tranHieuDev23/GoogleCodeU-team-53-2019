c3c764e8a589418094e369b70c64c13fe3f7f733
suyeon0506 <leesuyeon0506@gmail.com>
2019-06-02 18:37:04 +0900

    Expanded servlet logic & Added stat.html

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -7,9 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.codeu.data.Datastore;
+import com.google.gson.JsonObject;
+
 /**
  * Responds with a hard-coded message for testing purposes.
  */
+/*
 @WebServlet("/stats")
 public class StatsPageServlet extends HttpServlet{
   
@@ -20,3 +24,35 @@ public class StatsPageServlet extends HttpServlet{
   response.getOutputStream().println("hello world");
  }
 }
+*/
+/**
+ * Handles fetching site statistics.
+ */
+
+@WebServlet("/stats")
+public class StatsPageServlet extends HttpServlet{
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+   * Responds with site statistics in JSON.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    response.setContentType("application/json");
+
+    int messageCount = datastore.getTotalMessageCount();
+
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("messageCount", messageCount);
+    response.getOutputStream().println(jsonObject.toString());
+  }
+}
+
