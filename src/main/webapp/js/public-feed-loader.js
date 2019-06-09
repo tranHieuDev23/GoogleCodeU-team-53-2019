a03ae0648ac88b3d1df44a577e75ede5f773e9ed
longhoang08 <longhoang0811@gmail.com>
2019-06-09 18:56:47 +0700

    Fix public-feed-loader.js to display message in HTML format

--- src/main/webapp/js/public-feed-loader.js
+++ src/main/webapp/js/public-feed-loader.js
@@ -34,13 +34,13 @@ function buildMessageDiv(message) {
 
     const bodyDiv = document.createElement('div');
     bodyDiv.classList.add('message-body');
-    bodyDiv.appendChild(document.createTextNode(message.text));
+    //bodyDiv.appendChild(document.createTextNode(message.text));
+    bodyDiv.innerHTML = message.text;
 
     const messageDiv = document.createElement('div');
     messageDiv.classList.add("message-div");
     messageDiv.appendChild(headerDiv);
     messageDiv.appendChild(bodyDiv);
-
     return messageDiv;
 }
 
