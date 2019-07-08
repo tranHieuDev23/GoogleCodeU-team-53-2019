a7f10ce730a363030dafd16d27cd83f8253a7cb1
SUYEON LEE <40608847+suyeon0506@users.noreply.github.com>
2019-07-09 01:20:12 +0900

    Update VisionAPIHelper.java
    1. Changed label api name and deleted landmark api.
    2. Changed input & output as list and also changed some api process correspondingly.

--- src/main/java/com/google/codeu/controllers/visionapi/VisionAPIHelper.java
+++ src/main/java/com/google/codeu/controllers/visionapi/VisionAPIHelper.java
@@ -1,11 +1,12 @@
 package com.google.codeu.controllers.visionapi;
 
-//import java.io.InputStream;
+import java.io.InputStream;
 //import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Collections;
 
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
@@ -18,6 +19,8 @@ import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
 
 import com.google.protobuf.ByteString;
+import org.apache.commons.io.IOUtils;
+import java.lang.*;
 
 
 /**
@@ -45,76 +48,62 @@ public class VisionAPIHelper {
     /**
      * Predict relevant labels from a list of images, using the Vision API.
      * 
-     * @param imageStreams   Byte streams describing the input images.
+     * @param imageStreams List of byte streams describing the input images.
      * @param maxResultCount The maximum number of results to return.
      * @return An array of predicted labels, sorted by relevance.
      */
-    public List predictLabels(byte[] imageStreams, int maxResultCount) throws IOException {
-        List result = null;
+    public List<String> predictLabels(List<InputStream> imageStreams, int maxResultCount) throws IOException {
+        List<String> store = null;
+        List<String> result = null;
+        int length = imageStreams.size();
+        int count = 0;
+        int index = 0;
         /**
          * TODO: Implement label prediction, using Vision API here. There should not be
          * any duplicate in the result.
          */
-        
-        ByteString imageBytes = ByteString.copyFrom(imageStreams);
-        Image image = Image.newBuilder().setContent(imageBytes).build();
 
-        Feature feature = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
-        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feature).setImage(image).build();
-        List<AnnotateImageRequest> requests = new ArrayList<>();
-        requests.add(request);
+        //get api result for each image in the list
+        for(int i=0; i<length; i++){
+            byte[] imageBytesArray = IOUtils.toByteArray(imageStreams.get(i));
+            ByteString imageBytes = ByteString.copyFrom(imageBytesArray);
+            Image image = Image.newBuilder().setContent(imageBytes).build();
 
-        ImageAnnotatorClient client =ImageAnnotatorClient.create();
-        BatchAnnotateImagesResponse batchResponse = client.batchAnnotateImages(requests);
-        List<AnnotateImageResponse> imageResponses = batchResponse.getResponsesList();
-        AnnotateImageResponse imageResponse = imageResponses.get(0);
+            Feature feature = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feature).setImage(image).build();
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            requests.add(request);
 
-        if (imageResponse.hasError()) {
-            System.out.println("Error: " + imageResponse.getError().getMessage());
+            ImageAnnotatorClient client =ImageAnnotatorClient.create();
+            BatchAnnotateImagesResponse batchResponse = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> imageResponses = batchResponse.getResponsesList();
+            AnnotateImageResponse imageResponse = imageResponses.get(0);
+
+            if (imageResponse.hasError()) {
+                System.out.println("Error: " + imageResponse.getError().getMessage());
+            }
+
+            //store all result labels in form of "score+description"
+            for (EntityAnnotation annotation : imageResponse.getLabelAnnotationsList()) {
+                store.add(annotation.getScore() + "-" + annotation.getDescription());
+            }
         }
 
-        for (EntityAnnotation annotation : imageResponse.getLabelAnnotationsList()) {
-            result = Arrays.asList(annotation.getDescription());
-        }
+        //sort stored results by relevance order
+        Collections.sort(store, Collections.reverseOrder());
 
-        return result.subList(0, maxResultCount);
-    }
-
-    /**
-     * Predict relevant landmark infos from a list of images, using the Vision API.
-     * 
-     * @param imageStreams   Byte streams describing the input images.
-     * @return A string of predicted landmark infos, in form of "landmark , latitude: longitude:".
-     */
-    public String predictLandmarks(byte[] imageStreams) throws IOException {
-        String result = null;
-        /**
-         * TODO: Implement landmark prediction, using Vision API here. There should not be
-         * any duplicate in the result.
-         */
-        
-        ByteString imageBytes = ByteString.copyFrom(imageStreams);
-        Image image = Image.newBuilder().setContent(imageBytes).build();
-
-        Feature feature = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
-        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feature).setImage(image).build();
-        List<AnnotateImageRequest> requests = new ArrayList<>();
-        requests.add(request);
-
-        ImageAnnotatorClient client =ImageAnnotatorClient.create();
-        BatchAnnotateImagesResponse batchResponse = client.batchAnnotateImages(requests);
-        List<AnnotateImageResponse> imageResponses = batchResponse.getResponsesList();
-        AnnotateImageResponse imageResponse = imageResponses.get(0);
-
-        if (imageResponse.hasError()) {
-            System.out.println("Error: " + imageResponse.getError().getMessage());
+        //get decription(tags) without duplicates until 'count' reach 'maxResultCount'
+        while (count != maxResultCount){
+            String[] data = store.get(index).split("-");
+            if (result.contains(data[1]))
+                index += 1;
+            else{
+                result.add(data[1]);
+                count += 1;
+                index += 1;
+            }
         }
         
-        for (EntityAnnotation annotation : imageResponse.getLandmarkAnnotationsList()) {
-            LocationInfo info = annotation.getLocationsList().listIterator().next();
-            result = annotation.getDescription() + "," + info.getLatLng();
-        }
-
         return result;
     }
 }
