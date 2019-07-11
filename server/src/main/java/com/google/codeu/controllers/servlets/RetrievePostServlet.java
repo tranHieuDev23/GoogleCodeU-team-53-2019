f5d5375773a72fae1aeae9f206ce4d244a0adc51
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2019-07-11 10:24:49 +0700

    Added withComment param to RetrievePostServlet. Added index for comment query

--- server/src/main/java/com/google/codeu/controllers/servlets/RetrievePostServlet.java
+++ server/src/main/java/com/google/codeu/controllers/servlets/RetrievePostServlet.java
@@ -1,6 +1,7 @@
 package com.google.codeu.controllers.servlets;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
@@ -9,10 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.codeu.controllers.datastore.CommentDao;
 import com.google.codeu.controllers.datastore.PostDao;
+import com.google.codeu.models.Comment;
 import com.google.codeu.models.Post;
 import com.google.codeu.utils.ServletLink;
-import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @WebServlet(ServletLink.API_RETRIEVE_POST)
 public class RetrievePostServlet extends HttpServlet {
@@ -20,21 +25,32 @@ public class RetrievePostServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     private PostDao postDao;
-    private Gson gson;
+    private CommentDao commentDao;
 
     @Override
     public void init() throws ServletException {
         postDao = new PostDao();
-        gson = new Gson();
+        commentDao = new CommentDao();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        if (!req.getParameterMap().containsKey("postId")) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
         res.setContentType("application/json");
 
         UUID postId = UUID.fromString(req.getParameter("postId"));
-        Post result = postDao.getPost(postId);
+        Post post = postDao.getPost(postId);
+        JSONObject result = new JSONObject(post);
+        if (Boolean.parseBoolean(req.getParameter("withComment"))) {
+            List<Comment> comments = commentDao.getComments(postId);
+            result.put("comments", new JSONArray(comments));
+        }
 
-        res.getWriter().println(gson.toJson(result));
+
+        res.getWriter().println(result.toString());
     }
 }
