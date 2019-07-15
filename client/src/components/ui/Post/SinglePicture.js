3aeabc6b77ca5e952065ad1216762b23dd508ec6
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2019-07-15 17:04:21 +0700

    - Change image display from Carousel to Grid Gallery - Fixed bug about position: fixed

--- client/src/components/ui/Post/SinglePicture.js
+++ client/src/components/ui/Post/SinglePicture.js
@@ -1,27 +1,60 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import Gallery from "react-grid-gallery";
 
 class SinglePicture extends React.Component {
-  render() {
-    const item = [];
+  constructor(props) {
+    super(props);
+
     const { postImages } = this.props.post;
+    const promises = []
+    for (const [index, picture] of postImages.entries())
+      promises.push(getImageItem(picture.imageUrl.value, picture.imageDescription));
 
-    for (const [index, picture] of postImages.entries()) {
-      item.push(
-        <div key={index}>
-          <img src={picture.imageUrl.value} alt={picture.imageDescription} />
-          <p>{picture.imageDescription}</p>
-        </div>
-      );
-    }
+    Promise.all(promises).then((results) => {
+      console.log(results);
+      this.setState({
+        items: results
+      })
+    });
+  }
 
+  render() {
+    let galleryStyle = {
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    };
     return (
-      <Carousel showThumbs={false}>
-        {item}
-      </Carousel>
+      <React.Fragment>
+        {
+          (this.state == null) ? <div /> :
+            <Gallery
+              style={galleryStyle}
+              images={this.state.items}
+              rowHeight={240}
+              enableLightbox={true}
+              enableImageSelection={false} />
+        }
+      </React.Fragment>
     );
   }
+}
+
+function getImageItem(imageSrc, imageDescription) {
+  return new Promise((resolve) => {
+    let img = new Image();
+    img.onload = () => {
+      resolve({
+        src: imageSrc,
+        thumbnail: imageSrc,
+        thumbnailHeight: img.height,
+        thumbnailWidth: img.width,
+        alt: imageDescription,
+        caption: imageDescription
+      });
+    }
+    img.src = imageSrc;
+  });
 }
 
 export default SinglePicture;
