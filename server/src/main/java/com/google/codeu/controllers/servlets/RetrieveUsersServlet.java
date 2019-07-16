f6d5447e0e4d27f1d9f6cc87cef20f12864b09d3
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2019-07-16 23:04:44 +0700

    Created RetrieveUsersServlet

---
+++ server/src/main/java/com/google/codeu/controllers/servlets/RetrieveUsersServlet.java
@@ -0,0 +1,55 @@
+package com.google.codeu.controllers.servlets;
+
+import com.google.codeu.controllers.datastore.UserDao;
+import com.google.codeu.models.User;
+import com.google.codeu.utils.ServletLink;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(ServletLink.API_RETRIEVE_USERS)
+public class RetrieveUsersServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  private UserDao userDao;
+
+  @Override
+  public void init() throws ServletException {
+    userDao = new UserDao();
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    res.setContentType("application/json");
+    try {
+      String userIdsString = req.getParameter("userIds");
+      JSONArray userIds = new JSONArray(userIdsString);
+      List<User> result = new ArrayList<>();
+      for (int i = 0; i < userIds.length(); i++) {
+        String id = userIds.getString(i);
+        User user = userDao.getUser(id);
+        if (user == null)
+          throw new Exception("One of the userIds cannot be found in the database: " + id);
+        result.add(user);
+      }
+
+      JSONObject response = new JSONObject();
+      response.put("users", new JSONArray(result));
+      res.getWriter().write(response.toString());
+    } catch (Exception e) {
+      e.printStackTrace();
+      res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    }
+  }
+}
