257fcae9ddc62a2ffc6e7ff07c44287be87da6b5
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2019-07-19 23:36:52 +0700

    Changed response form

--- server/src/main/java/com/google/codeu/controllers/servlets/PredictTagsServlet.java
+++ server/src/main/java/com/google/codeu/controllers/servlets/PredictTagsServlet.java
@@ -3,8 +3,8 @@ package com.google.codeu.controllers.servlets;
 //import com.google.codeu.controllers.datastore.TagDao;
 import com.google.codeu.controllers.visionapi.VisionAPIHelper;
 import com.google.codeu.utils.ServletLink;
-import com.google.gson.Gson;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -26,21 +26,20 @@ public class PredictTagsServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    //private TagDao tagDao;
+    // private TagDao tagDao;
     private VisionAPIHelper visionHelper;
-    private Gson gson;
 
     @Override
     public void init() throws ServletException {
-        //tagDao = new TagDao();
+        // tagDao = new TagDao();
         visionHelper = VisionAPIHelper.getInstance();
-        gson = new Gson();
     }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("application/json");
-        
-        List<String> result = null; 
+
+        List<String> result = null;
         try {
             result = getPredTagsFromReq(req);
         } catch (Exception e) {
@@ -48,28 +47,30 @@ public class PredictTagsServlet extends HttpServlet {
             e.printStackTrace();
             res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
-        res.getWriter().println(gson.toJson(result));
+
+        JSONObject response = new JSONObject();
+        response.put("tags", new JSONArray(result));
+        res.getWriter().println(response);
     }
 
-    private List<String> getPredTagsFromReq(HttpServletRequest req) throws IOException, ServletException {
+    private List<String> getPredTagsFromReq(HttpServletRequest req) throws Exception {
         /** want to change for the client to change the limit */
         int limit = 5; // maximum number of tags to return
-        //UUID postId = imageDao.getPostId;
-       
+        // UUID postId = imageDao.getPostId;
+
         String postDetailsJson = req.getParameter("postDetails");
         if (postDetailsJson == null)
-            return null;
+            throw new RuntimeException("There is no postDetails param!");
         JSONObject postDetails = new JSONObject(postDetailsJson);
-
         int imageCount = postDetails.getInt("numberOfImages");
 
-        //numberOfImages
+        // numberOfImages
         List<InputStream> imageStreams = new ArrayList<>();
-        
-        //file-0, file-1 etc format from client request
+
+        // file-0, file-1 etc format from client request
         for (int i = 0; i < imageCount; i++) {
             try {
-                Part filePart = req.getPart("file-"+i);
+                Part filePart = req.getPart("file-" + i);
                 InputStream fileContent = filePart.getInputStream();
                 imageStreams.add(fileContent);
             } catch (Exception e) {
@@ -80,9 +81,9 @@ public class PredictTagsServlet extends HttpServlet {
 
         /** Call vision API Helper to predict tags on images */
         List<String> PredictedTags = visionHelper.predictTags(imageStreams, limit);
-        
-        //PostTags result = new PostTags(PredictedTags);
+
+        // PostTags result = new PostTags(PredictedTags);
         return PredictedTags;
-        //res.getWriter().println(gson.toJson(PredictedTags));
+        // res.getWriter().println(gson.toJson(PredictedTags));
     }
 }
